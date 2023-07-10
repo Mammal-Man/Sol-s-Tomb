43d33959a4edce2a2b5cd2b50a59d455d5f86963
Mammal-Man <139150865+Mammal-Man@users.noreply.github.com>
2023-07-10 09:02:39 -0700

    Allowing player to jump

--- My project/Assets/Scripts/PlayerControl.cs
+++ My project/Assets/Scripts/PlayerControl.cs
@@ -6,15 +6,19 @@ public class PlayerControl : MonoBehaviour
 {
     public float inputHoriz;
     public bool isOnGround;
+    private Rigidbody2D playerRb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode2D.Impulse);
+        }
     }
 }
