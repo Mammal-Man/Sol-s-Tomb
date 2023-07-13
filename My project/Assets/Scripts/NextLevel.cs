e6adc4d7dea6b213c6bbeff8fc16486bfa0f351f
Mammal-Man <139150865+Mammal-Man@users.noreply.github.com>
2023-07-13 07:56:41 -0700

    Manual Level Progress

--- My project/Assets/Scripts/NextLevel.cs
+++ My project/Assets/Scripts/NextLevel.cs
@@ -14,9 +14,15 @@ public class NextLevel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            nextLevel = true;
+        }
+
         if (nextLevel == true)
         {
-            transform.position = new Vector3(transform.position.x + 23, 0);
+            transform.position = new Vector3(transform.position.x - 23, 0);
             nextLevel = false;
         }
     }
